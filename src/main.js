4608ea8f19ef2e3c4694b49f8548e81e265dd77c
Vladimir <zpoison1993@gmail.com>
2019-03-30 12:25:58 +0300

    mobile-menu added

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
 
+
 import "./scripts/parallax";
+import "./scripts/mobile-menu";
 import "./scripts/skills";
 import "./scripts/works";
 import "./scripts/reviews";
